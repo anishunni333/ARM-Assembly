dfa44018b3a956f16ae8040d370b4806539101d3
anishunni333 <33221363+anishunni333@users.noreply.github.com>
2017-11-04 15:26:31 +0530

    Add files via upload

---
+++ gcd.s
@@ -0,0 +1,16 @@
+     PRESERVE8
+     THUMB
+     AREA     appcode, CODE, READONLY
+     EXPORT __main
+	 ENTRY 
+__main  FUNCTION		 
+    MOV r2,#1;
+	MOV r7,#1;
+loop	CMP r2,r7;
+	SUBGT r2,r2,r7;
+	SUBLT r7,r7,r2;
+	BNE loop
+       
+stop B stop ; stop program
+     ENDFUNC
+     END
